a367340d62ef7fbf0eef8ff4d66b94df5fb5f491
Ahmed <hamoda9355@yahoo.com>
2019-09-24 23:42:51 +0200

    Initial Commit

---
+++ KhrogatyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KhrogatyApp
+//
+//  Created by Ahmed Ibrahim on 9/24/19.
+//  Copyright © 2019 Ahmed Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
